15455f94c148c9bd0d58aa365472c593aac9c3a9
Ryan Miller <ronveto14@gmail.com>
2023-05-11 18:26:18 -0500

    Project 1 Files

--- project1_controller.py
+++ project1_controller.py
@@ -1,4 +1,7 @@
 from PyQt5.QtWidgets import *
+from PyQt5.QtCore import QDir, QUrl
+from PyQt5.QtMultimedia import QMediaContent, QMediaPlayer
+from pathlib import Path
 from project1_view import *
 
 QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
@@ -7,11 +10,10 @@ QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True)
 
 class Controller(QMainWindow, Ui_mainWindow):
 
-    PWR_STATUS = 'OFF'
     MIN_VOLUME = 0
-    MAX_VOLUME = 10
+    MAX_VOLUME = 100
     MIN_CHANNEL = 1
-    MAX_CHANNEL = 3
+    MAX_CHANNEL = 5
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -19,7 +21,7 @@ class Controller(QMainWindow, Ui_mainWindow):
 
         self.__status = False
         self.__muted = False
-        self.__volume = Controller.MIN_VOLUME
+        self.__volume = Controller.MAX_VOLUME
         self.__channel = Controller.MIN_CHANNEL
 
         self.button_power.clicked.connect(lambda: self.power())
@@ -29,53 +31,102 @@ class Controller(QMainWindow, Ui_mainWindow):
         self.button_channel_up.clicked.connect(lambda: self.channel_up())
         self.button_channel_down.clicked.connect(lambda: self.channel_down())
         self.button_channel_list.clicked.connect(lambda: self.channel_list())
+        self.numpad_0.clicked.connect(lambda: self.numpad_pressed(0))
+        self.numpad_1.clicked.connect(lambda: self.numpad_pressed(1))
+        self.numpad_2.clicked.connect(lambda: self.numpad_pressed(2))
+        self.numpad_3.clicked.connect(lambda: self.numpad_pressed(3))
+        self.numpad_4.clicked.connect(lambda: self.numpad_pressed(4))
+        self.numpad_5.clicked.connect(lambda: self.numpad_pressed(5))
+        self.numpad_6.clicked.connect(lambda: self.numpad_pressed(6))
+        self.numpad_7.clicked.connect(lambda: self.numpad_pressed(7))
+        self.numpad_8.clicked.connect(lambda: self.numpad_pressed(8))
+        self.numpad_9.clicked.connect(lambda: self.numpad_pressed(9))
 
+
+        self.mediaPlayer = QMediaPlayer(None, QMediaPlayer.VideoSurface)
+        videoWidget = QVideoWidget()
+        widget = QWidget(self)
+        widget.setGeometry(QtCore.QRect(10, 0, 390, 210))
+        layout = QVBoxLayout()
+        layout.addWidget(videoWidget)
+        widget.setLayout(layout)
+        self.mediaPlayer.setVideoOutput(videoWidget)
 
     def power(self):
         self.__status = not self.__status
         if self.__status == True:
             Controller.PWR_STATUS = 'ON'
+            self.mediaPlayer.setMedia(QMediaContent(QUrl.fromLocalFile(str((Path(__file__).with_name(f'channel{self.__channel}.wmv')).absolute()))))
+            self.mediaPlayer.play()
+            self.label_info.setText(f'Channel = {self.__channel}  Volume = {self.mediaPlayer.volume()}')
         elif self.__status == False:
             Controller.PWR_STATUS = 'OFF'
-        self.label_info.setText(f'Power = {Controller.PWR_STATUS}\nChannel = {self.__channel}\nVolume = {self.__volume}')
+            self.mediaPlayer.stop()
+            self.label_info.setText('')
+        self.label_power.setText(f'Power = {Controller.PWR_STATUS}')
 
     def volume_up(self):
-        self.__muted = False
         if self.__status == True:
-            self.__volume += 1
+            self.mediaPlayer.setMuted(False)
+            self.__volume += 20
+            self.mediaPlayer.setVolume(self.__volume)
             if self.__volume > Controller.MAX_VOLUME:
                 self.__volume = Controller.MAX_VOLUME
-            self.label_info.setText(f'Power = {Controller.PWR_STATUS}\nChannel = {self.__channel}\nVolume = {self.__volume}')
+            self.label_info.setText(f'Channel = {self.__channel}  Volume = {self.mediaPlayer.volume()}')
 
     def volume_down(self):
-        self.__muted = False
         if self.__status == True:
-            self.__volume -= 1
+            self.mediaPlayer.setMuted(False)
+            self.__volume -= 20
+            self.mediaPlayer.setVolume(self.__volume)
             if self.__volume < Controller.MIN_VOLUME:
                 self.__volume = Controller.MIN_VOLUME
-            self.label_info.setText(f'Power = {Controller.PWR_STATUS}\nChannel = {self.__channel}\nVolume = {self.__volume}')
+            self.label_info.setText(f'Channel = {self.__channel}  Volume = {self.mediaPlayer.volume()}')
 
     def mute(self):
-        if self.__status == True:
-            self.__muted = not self.__muted
-            self.label_info.setText(f'Power = {Controller.PWR_STATUS}\nChannel = {self.__channel}\nVolume = 0')
+        if self.__status == True and self.mediaPlayer.isMuted() == False:
+            self.mediaPlayer.setMuted(True)
+            self.mediaPlayer.setVolume(0)
+            self.label_info.setText(f'Channel = {self.__channel}  Volume = {self.mediaPlayer.volume()}')
+        elif self.__status == True and self.mediaPlayer.isMuted() == True:
+            self.mediaPlayer.setMuted(False)
+            self.label_info.setText(f'Channel = {self.__channel}  Volume = {self.mediaPlayer.volume()}')
 
     def channel_up(self):
         if self.__status == True:
             self.__channel += 1
             if self.__channel > Controller.MAX_CHANNEL:
                 self.__channel = Controller.MIN_CHANNEL
-            self.label_info.setText(f'Power = {Controller.PWR_STATUS}\nChannel = {self.__channel}\nVolume = {self.__volume}')
+            self.mediaPlayer.setMedia(QMediaContent(
+                QUrl.fromLocalFile(str((Path(__file__).with_name(f'channel{self.__channel}.wmv')).absolute()))))
+            self.mediaPlayer.play()
+            self.label_info.setText(f'Channel = {self.__channel}  Volume = {self.mediaPlayer.volume()}')
 
     def channel_down(self):
         if self.__status == True:
             self.__channel -= 1
             if self.__channel < Controller.MIN_CHANNEL:
                 self.__channel = Controller.MAX_CHANNEL
-            self.label_info.setText(f'Power = {Controller.PWR_STATUS}\nChannel = {self.__channel}\nVolume = {self.__volume}')
+            self.mediaPlayer.setMedia(QMediaContent(
+                QUrl.fromLocalFile(str((Path(__file__).with_name(f'channel{self.__channel}.wmv')).absolute()))))
+            self.mediaPlayer.play()
+            self.label_info.setText(f'Channel = {self.__channel}  Volume = {self.mediaPlayer.volume()}')
 
+    def numpad_pressed(self, numpad_num):
+        if self.__status == True:
+            if numpad_num in range(Controller.MIN_CHANNEL, Controller.MAX_CHANNEL + 1):
+                if self.__channel != numpad_num:
+                    self.__channel = numpad_num
+                    self.mediaPlayer.setMedia(QMediaContent(
+                        QUrl.fromLocalFile(str((Path(__file__).with_name(f'channel{numpad_num}.wmv')).absolute()))))
+                    self.mediaPlayer.play()
+                    self.label_info.setText(f'Channel = {self.__channel}  Volume = {self.mediaPlayer.volume()}')
+                else:
+                    pass
+            else:
+                self.label_info.setText(f'Channel {numpad_num} not currently available')
     def channel_list(self):
         if self.__status == True:
-            self.label_info.setText('Available 1-10')
+            self.label_info.setText('Channels Available: 1-5')
 
 
